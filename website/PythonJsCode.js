92bca665acb803a35f69d2cd33c7ebff68abe222
CrimsonGlacier <120449326+CrimsonGlacier@users.noreply.github.com>
2023-04-13 01:14:07 -0700

    Create PythonJsCode.js

---
+++ website/PythonJsCode.js
@@ -0,0 +1,13 @@
+fetch("https://pythonviewcounter.azurewebsites.net/api/HttpTrigger1PYTHON?")
+.then((res) => res.json())
+.then((data) => {
+  const viewCount = data.viewCount; // Access the viewCount property
+  console.log("View Count:", viewCount); // Output the viewCount number
+  var header=document.getElementById("testr");
+  document.querySelector("h1").append(data.viewCount);
+  const cors = require("cors")
+  app.use(cors)})
+  
+.catch((error) => {
+  console.error("Error:", error); // Handle any errors that may occur
+})
